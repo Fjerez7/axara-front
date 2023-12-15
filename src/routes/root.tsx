fbdb4f3f1d4d18ba9a7fed4eba9fdb8965b8dbec
Fjerez7 <fjerezm07@gmail.com>
2023-12-14 19:23:25 -0500

    Add account page

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,13 @@
-import {useRoutes } from 'react-router-dom'
+import {useRoutes} from 'react-router-dom'
 import ErrorPage from "./errorPage.tsx";
 import HomePage from "../pages/Home/HomePage.tsx";
 import CollectionPage from "../pages/Collection/CollectionPage.tsx";
 import CartPage from "../pages/Cart/CartPage.tsx";
 import LoginPage from "../pages/Login/LoginPage.tsx";
 import SignupPage from "../pages/Signup/SignupPage.tsx";
+import AccountPage from "../pages/Account/AccountPage.tsx";
+import AccountDetailsPage from "../pages/AccountDetails/AccountDetailsPage.tsx";
+import AccountChangePasswordPage from "../pages/AccountChangePassword/AccountChangePasswordPage.tsx";
 
 export default function Root() {
     const routes = useRoutes([
@@ -29,10 +32,23 @@ export default function Root() {
             element: <SignupPage/>
         },
         {
+            path:'/account',
+            element: <AccountPage userName={'Fabian'}/>,
+        },
+        {
+            path: '/account/details',
+            element: <AccountDetailsPage/>
+        },
+        {
+            path: '/account/change-password',
+            element: <AccountChangePasswordPage/>
+        },
+        {
             path:'*',
             element: <ErrorPage/>
         }
     ])
 
     return routes
+
 }
