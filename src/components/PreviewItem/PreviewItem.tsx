f0c8d97bd2adc4dbd949cb3a7014bbc7031ea61d
Fjerez7 <fjerezm07@gmail.com>
2024-05-30 12:25:33 -0500

    add more features

--- src/components/PreviewItem/PreviewItem.tsx
+++ src/components/PreviewItem/PreviewItem.tsx
@@ -1,5 +1,5 @@
-import {Card} from "primereact/card";
-import {FC} from "react";
+
+import {FC, useState} from "react";
 import styles from './PreviewItem.module.css'
 import {Link} from "react-router-dom";
 
@@ -8,17 +8,38 @@ interface PreviewItemProps {
     name: string
     size: 'sm' | 'lg'
     urlToNavigate?: string
+    paramForUrl?: number
 }
 
-export const PreviewItem:FC<PreviewItemProps> = ({image,name,size,urlToNavigate = ''}) => {
+export const PreviewItem:FC<PreviewItemProps> = ({image,name,size,urlToNavigate = `/collections/`,paramForUrl }) => {
+    const urlImage = `http://localhost:8080/api/v1/products/images/${image}`
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
     return(
-        <Link to={urlToNavigate}>
-        <Card className={styles.card} style={{
-            width: `${size == 'lg'? '750px': '350px'}`,
-            height: `${size == 'lg'? '1000px': '450px'}`
-        }}>
-            <img src={image} alt={`collection ${name}`} />
-        </Card>
+        <Link to={urlToNavigate + paramForUrl}>
+            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={styles.card} style={{
+                width: `${size == 'lg' ? '750px' : '350px'}`,
+                height: `${size == 'lg' ? '1000px' : '450px'}`
+            }}>
+                <div className={styles.divForImage}>
+                    <img src={urlImage} alt={name} style={{
+                        width: `${size == 'lg' ? '700px' : '300px'}`,
+                        height: '100%',
+                    }}/>
+                    {isHovered && (
+                        <div className={styles.poster}>
+                        <h3>{name}</h3>
+                    </div>
+                    )}
+                </div>
+            </div>
         </Link>
     )
 }
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,20 +1,33 @@
 import {Layout} from "../../components/Layout/Layout.tsx";
 import {PreviewItem} from "../../components/PreviewItem/PreviewItem.tsx";
 import styles from './HomePage.module.css'
+import {useQuery} from "@tanstack/react-query";
+import {getAllProducts} from "../../queries/Products.ts";
+import {Product} from "../../types/Products.ts";
 
 const HomePage = () => {
+    const {data} = useQuery({
+        queryKey: ['products'],
+        queryFn: getAllProducts
+    })
     return(
         <Layout>
             <div>
                 <div className={styles.sectionOne}>
-                    <PreviewItem name={''} image={''} size={"lg"}/>
-                    <PreviewItem name={''} image={''} size={"lg"}/>
+                    {data && data.slice(0,2).map((product:Product) => {
+                        const imgUid = product.images[0].uid;
+                        return (
+                            <PreviewItem key={product.id} image={imgUid} name={product.name} size={"lg"} paramForUrl={product.id}/>
+                        )
+                    })}
                 </div>
                 <div className={styles.sectionTwo}>
-                    <PreviewItem name={''} image={''} size={"sm"}/>
-                    <PreviewItem name={''} image={''} size={"sm"}/>
-                    <PreviewItem name={''} image={''} size={"sm"}/>
-                    <PreviewItem name={''} image={''} size={"sm"}/>
+                    {data && data.slice(2,7).map((product:Product) => {
+                        const imgUid = product.images[0].uid;
+                        return (
+                            <PreviewItem key={product.id} image={imgUid} name={product.name} size={"sm"} paramForUrl={product.id}/>
+                        )
+                    })}
                 </div>
             </div>
         </Layout>
